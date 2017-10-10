f7fcfb667be949412650bc2f65a83f0a64590e5b
AaronFlower <haojunzhan@gmail.com>
2017-10-10 21:30:13 +0800

    add init data

--- sentdex/00-regression.py
+++ sentdex/00-regression.py
@@ -4,27 +4,46 @@ import pandas as pd
 import quandl
 import math
 
-quandl.ApiConfig.api_key = '8bnp57r24GpxbF-aCf1A'
+api_key = open('../quandl-token.txt', 'r').read()
+quandl.ApiConfig.api_key = api_key
 
 
-#dataframe
-df = quandl.get("WIKI/GOOGL")
-# print(df.head())
+def grap_fresh_data():
+	''' 将拉取的数据存储下来 '''
+	quandl.ApiConfig.api_key = api_key
+	df = quandl.get('WIKI/GOOGL')
+	df.to_pickle('GOOGLE-2017-10-09.pickle')
+
+def load_data():
+	'''读取原始数据'''
+	df = pd.read_pickle('GOOGLE-2017-10-09.pickle')
+	return df
+
+def get_adjusted_data(df):
+	'''我们 Adjusted Price 对我们更重要'''
+	df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+	# 把一天内的 volatility 考虑进来，
+	# high - low percent change
+	df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+	df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+	df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+	return df
+# grap_fresh_data() # 2017-10-09
+
+df = load_data()
+df = get_adjusted_data(df)
+print(df.head())
+
+
+# forecast_col = 'Adj. Close'
+# df.fillna(-99999, inplace=True)
+
+# # test samples. 10% 的测试用例
+# forcast_out = int(math.ceil(0.01 * len(df)))
+
+# df['label'] = df[forecast_col].shift(-forcast_out)
+# df.dropna(inplace=True)
+
+# print(df)
 # 
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
-df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
-df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-print(df.head())
-
-forecast_col = 'Adj. Close'
-df.fillna(-99999, inplace=True)
-
-# test samples. 10% 的测试用例
-forcast_out = int(math.ceil(0.01 * len(df)))
-
-df['label'] = df[forecast_col].shift(-forcast_out)
-df.dropna(inplace=True)
-
-print(df.head())
