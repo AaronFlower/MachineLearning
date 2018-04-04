290b0cc20723057380e0feaa7fb351d5c81f8771
AaronFlower <haojunzhan@gmail.com>
2018-04-04 08:40:34 +0800

    add simple test unit

--- 00-linear-regression-c/src/test.cpp
+++ 00-linear-regression-c/src/test.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 #include <cstddef>
+#include <iomanip>      // std::setprecision
 #include "utils.h"
 
 using namespace std;
 
+static int testCount = 0;
+static int testPass = 0;
+static int mainRet  = 0;
+
+/**
+ * 自已定义一个简单的测试框架。
+ */
+#define EXPECT_EQUAL_BASE(expect, actual)\
+	do {\
+		testCount++;\
+		if ((expect) == (actual)) {\
+			testPass++;\
+		} else {\
+			cerr << __FILE__ << ":" << __LINE__ << ": expect: " << (expect) << " actual: " << (actual) << endl;\
+			mainRet = 1;\
+		}\
+	}while(0)
+
 void testArraySum()
 {
-	double arr[] = {1, 2, 3};
-	cout << Utils::array_sum(arr, 10);
- // 	cout << Utils::array_sum(NULL, 10);
+	double arr[] = {1, 2, 3, -3, -2, -1};
+	size_t len = sizeof(arr) / sizeof(arr[0]);
+	EXPECT_EQUAL_BASE(6, Utils::array_sum(arr, 3));
+	EXPECT_EQUAL_BASE(0, Utils::array_sum(arr, len));
+	EXPECT_EQUAL_BASE(0, Utils::array_sum(arr, 0));
 }
+
 int main() {
-	cout << "Hello world\n" ;
-	sayHi();	
 	testArraySum();
-	return 0;
+	double percentage = testPass * 100.0 / testCount;
+	cout << fixed << setprecision(2);
+	cout << testPass << "/" << testCount << "(" << percentage << "%) passed \n";
+	return mainRet;
 }
